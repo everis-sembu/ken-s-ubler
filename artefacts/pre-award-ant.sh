dd434b26dda335b5e5ed6fe24a2dd4d18ad244fc
everis-sembu <paulakeen@gmail.com>
2019-03-31 18:07:39 +0200

    ant moved

--- artefacts/pre-award-ant.sh
+++ artefacts/pre-award-ant.sh
@@ -5,7 +5,7 @@ exit
 fi
 
 echo Building package...
-java -Dant.home=./bin/ant -classpath ./lib/saxon-6.5.5.jar:./bin/ant/lib/ant-launcher.jar:./lib/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile ./produceUBLschemas.xml -Ddir=./util/ -Dutilitydir=./util/ -Dworkdir=./work/ "-Dpackage=2.3" -DUBLversion=2.3 -DUBLprevVersion=2.2 -Drawdir=pre-award-raw-20181027-1910z -Dstage=$1 -Dversion=$2 -Ddatetimelocal=$3 $4
+java -Dant.home=./lib/ant -classpath ./lib/saxon-6.5.5.jar:./lib/ant/lib/ant-launcher.jar:./lib/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile ./produceUBLschemas.xml -Ddir=./util/ -Dutilitydir=./util/ -Dworkdir=./work/ "-Dpackage=2.3" -DUBLversion=2.3 -DUBLprevVersion=2.2 -Drawdir=pre-award-raw-20181027-1910z -Dstage=$1 -Dversion=$2 -Ddatetimelocal=$3 $4
 
 if [ "$?" != "0" ]; then exit ; fi
 b
