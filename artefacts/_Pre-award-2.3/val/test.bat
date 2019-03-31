6775f6567032f18bfe773f27d3382092db07bb7b
everis-sembu <paulakeen@gmail.com>
2019-03-31 13:41:50 +0200

    version 0.1

--- artefacts/_Pre-award-2.3/val/test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call validate ..\xsd\maindoc\UBL-Order-2.3-Pre-award.xsd order-test-good.xml
-call validate ..\xsd\maindoc\UBL-Order-2.3-Pre-award.xsd order-test-bad1.xml
-call validate ..\xsd\maindoc\UBL-Order-2.3-Pre-award.xsd order-test-bad2.xml
